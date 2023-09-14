09a748554369dded0a5476743197a8eeab051e85
Dongmin Kim <macaw2469@gmail.com>
2023-09-14 17:05:21 +0900

    추가 : koa 서버
        - koa를 이용하여 간단한 서버 구축
        - 서버 실행 시 koa가 not found Module이라서 npm으로 다시 koa설치
        - port 4000으로 서버 구축

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,11 @@
-const hello = "hello";
+
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(4000, ()=>{
+    console.log("Listening to port 4000");
+})

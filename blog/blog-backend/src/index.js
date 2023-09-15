bb07522919ef5b2218840713a0b7d99bcc808c0b
Dongmin Kim <macaw2469@gmail.com>
2023-09-15 12:24:14 +0900

    추가 : Koa미들웨어 use 사용

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,7 +2,25 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(ctx => {
+app.use(async (ctx,next)=>{
+  console.log(ctx.url);
+  console.log(1);
+  if( ctx.query.authorized !== '1'){
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  await next();
+  console.log('END')
+  
+})
+
+app.use((ctx,next)=>{
+  console.log(2);
+  next();
+})
+
+
+app.use(ctx => { // 미들웨어 함수를 애플리케이션에 등록하는 .use , 안에 들어있는 arrow func이 미들웨어 함수
   ctx.body = 'Hello World';
 });
 

6c851f8f0d76b129fc8b6c750b63c7168f761a25
Dongmin Kim <macaw2469@gmail.com>
2023-09-25 19:59:19 +0900

    추가 :  mongoose 적용
    - mongoose와 dotenv가 적용이 안되어 npm으로 재설치
    - 환경변수 파일 생성 : .env
    - index.js에서 dotenv를 이용하여 서버 생성
    - index.js에서 mongoose를 이용하여 서버와 데이터베이스 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,20 @@
+require('dotenv').config(); 
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser'); // Request의 body에 JSON형태로 데이터를 넣어 주는 패키지
+const mongoose = require('mongoose');
+
+const {PORT, MONGO_URI} = process.env;
+
+mongoose
+.connect(MONGO_URI)
+.then(()=>{
+  console.log("Connected to MongoDB")
+})
+.catch(e=>{
+  console.log(e);
+});
+
 
 // 라우터 가져오기
 const api = require('./api')
@@ -15,23 +29,11 @@ router.use('/api', api.routes()); // api 라우터 적용
 app.use(bodyParser());
 
 
-router.get('/', ctx=>{
-  ctx.body = '홈'
-})
-
-router.get('/about/:name?', ctx=>{
-  const {name} = ctx.params; // 파라미터 조회
-  ctx.body = name ? `${name}의 소개` : `그냥 소개`
-});
-
-router.get('/posts', ctx=>{
-  const {id} = ctx.query;
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없음'
-})
-
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, ()=>{
-    console.log("Listening to port 4000");
+const port = PORT || 4000;
+app.listen(port, ()=>{
+    console.log("Listening to port %d", port);
 })
+

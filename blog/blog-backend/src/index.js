f8f00a36988ca391f11dcf8186fddee66bc7e298
Dongmin Kim <macaw2469@gmail.com>
2023-09-26 10:03:27 +0900

    추가 : esm모듈을 사용하기 위한 기본 설정
    - yarn add esm : esm 모듈 추가
    - 기존 index.js를 main.js로 이름 변경
    - 새로운 index.js 파일 생성
    - esm문법 ( import / export )를 사용하기 위해 package.json과 .eslintrc.json파일 수정

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,39 +1,5 @@
-require('dotenv').config(); 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser'); // Request의 body에 JSON형태로 데이터를 넣어 주는 패키지
-const mongoose = require('mongoose');
 
-const {PORT, MONGO_URI} = process.env;
-
-mongoose
-.connect(MONGO_URI)
-.then(()=>{
-  console.log("Connected to MongoDB")
-})
-.catch(e=>{
-  console.log(e);
-});
-
-
-// 라우터 가져오기
-const api = require('./api')
-
-const app = new Koa();
-const router = new Router();
-
-// 라우터 설정
-router.use('/api', api.routes()); // api 라우터 적용
-
-// 라우터 적용 전에 bodyParser를 적용해야 한다. 왜?
-app.use(bodyParser());
-
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-const port = PORT || 4000;
-app.listen(port, ()=>{
-    console.log("Listening to port %d", port);
-})
-
+// 이 파일에서만 no - global-assign ESLint 옵션을 비활성화합니다.
+/* eslint-disable no-global-assign */ // <- 이 주석이 비활성화 주석이다.
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');

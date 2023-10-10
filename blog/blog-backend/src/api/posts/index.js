40a8ea944452ccb3574fdceec66b7cd4638c7ba3
Dongmin Kim <macaw2469@gmail.com>
2023-10-10 11:48:53 +0900

    추가 : 데이터 삭제 및 수정
        - posts.ctrl.js에서 remove, update함수를 구현
        - src/api/posts/index.js에서 삭제 및 업데이트 함수 라우팅

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -7,7 +7,7 @@ const posts  = new Router();
 posts.post('/', postsCtrl.write);
 posts.get('/', postsCtrl.list);
 posts.get('/:id', postsCtrl.read);
-// posts.delete('/:id', postsCtrl.remove);
-// posts.patch('/:id', postsCtrl.update);
+posts.delete('/:id', postsCtrl.remove);
+posts.patch('/:id', postsCtrl.update);
 
 export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -52,6 +52,32 @@ export const read = async ctx =>{
     }
 }
 
-// export const remove = ctx => {}
+// 해당 id 삭제
+export const remove = async ctx => {
+    const {id} = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204 // 성공했지만 응답데이터 없음을 의미
+    } catch (error) {
+        ctx.throw(500, error)
+    }
+}
 
-// export const update = ctx => {}
+// 업데이트
+export const update = async ctx => {
+    const {id} = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true,
+            // true는 업데이트된 데이터, false는 업데이트 전 데이터
+        }).exec();
+
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (error) {
+        ctx.throw(500, error)
+    }
+}

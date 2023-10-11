83e39977f065492fdf4056c86caaad5f5cb861e3
Dongmin Kim <macaw2469@gmail.com>
2023-10-11 15:04:11 +0900

    추가 : 모델 메서드로 비밀번호 설정 및 확인 ( 인스턴스 메서드 방식 )

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,11 +1,26 @@
 // jwt를 사용한 인증 시스템 모델 user
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt' // 비밀번호를 단방향 해싱 함수로 사용하기 위한 bcrypt 라이브러리
+
 
 // User 스키마
 const UserSchema = new Schema({
     username : String,
     hashedPassword : String
 });
+
+// 모델 메소드 : 인스턴스 메서드
+// 모델을 통해 만든 문서의 인스턴스로 사용할 수 있는 함수
+// 방식 : 모델인스턴스.method.메소드이름 = 함수
+UserSchema.method.setPassword = async function(password){
+    const hash = await bcrypt.hash(password, 10);
+    this.hashedPassword = hash
+}
+
+UserSchema.method.checkPassword = async function(password){
+    const result = await bcrypt.compare(password, this.hashedPassword);
+    return result; // t & f
+}
 
 const User = mongoose.model('User', UserSchema);
 export default User;

6547d2fb5bde41cf04e2d68c54402d35a423215a
Dongmin Kim <macaw2469@gmail.com>
2023-10-11 18:43:10 +0900

    추가 : 회원 가입 구현
        - register 함수 구현
        - serialize 구현

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,7 +1,67 @@
 // 회원 인증 API
+import Joi from 'joi';
+import User from '../../models/user';
+
+
+/** POST / api / auth / register
+ * 
+ * 데이터 구조
+ * {
+ *  username : 'kim',
+ *  password : '123444aadf'
+ * 
+ * }
+ * 
+ */
+
 
 // 회원 가입
 export const register = async ctx=>{
+    
+    // 회원가입 시 전송된 Request Body 검증
+    const schema = Joi.object().keys({
+        username : Joi.string()
+            .alphanum()
+            .min(3)
+            .max(20)
+            .required(),
+        password : Joi.string().required(),
+    })
+    const result = schema.validate(ctx.request.body);
+
+    if(result.error){
+        ctx.status = 400;
+        ctx.body = result.error;
+        return
+    }
+
+    // 검증 후 DB에 회원 정보 입력
+    const { username , password } =ctx.request.body;
+    try {
+        // 이미 존재하는 아이디인지 확인
+        const exists = await User.findByUsername(username)
+        if(exists){
+            ctx.status = 409 // conflict : 충돌
+            return;
+        }
+
+        const user = new User({
+            username,
+        });
+        await user.setPassword(password);
+        await user.save(); // DB에 저장
+
+        // 응답할 데이터에서 hashedPassword제거
+        // const data = user.toJSON()
+        // delete data.hashedPassword;
+        // ctx.body = data;
+        // !! serialize로 대처
+        ctx.body = user.serialize();
+        
+        
+    } catch (error) {
+        ctx.throw(500, error)
+    }
 
 }
 
@@ -17,5 +77,5 @@ export const check = async ctx=>{
 
 // 로그아웃
 export const logout = async ctx=>{
-    
+
 }
--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -6,18 +6,18 @@ import bcrypt from 'bcrypt' // 비밀번호를 단방향 해싱 함수로 사용
 // User 스키마
 const UserSchema = new Schema({
     username : String,
-    hashedPassword : String
+    hashedPassword : String,
 });
 
 // 모델 메소드 : 인스턴스 메서드
 // 모델을 통해 만든 문서의 인스턴스로 사용할 수 있는 함수
 // 방식 : 모델인스턴스.method.메소드이름 = 함수
-UserSchema.method.setPassword = async function(password){
+UserSchema.methods.setPassword = async function(password){
     const hash = await bcrypt.hash(password, 10);
-    this.hashedPassword = hash
+    this.hashedPassword = hash;
 }
 
-UserSchema.method.checkPassword = async function(password){
+UserSchema.methods.checkPassword = async function(password){
     const result = await bcrypt.compare(password, this.hashedPassword);
     return result; // t & f
 }
@@ -25,11 +25,19 @@ UserSchema.method.checkPassword = async function(password){
 // 모델 메서드 : 스태틱 메서드
 // 모델에서 바로 사용할 수 있는 함수
 // 방식 : 모델인스턴스.static.메서드이름 = 함수
-UserSchema.static.findByUsername = function(username){
+UserSchema.statics.findByUsername = function(username){
     return this.findOne({username})
 }
 
 // ? 인스턴스와 스태틱의 차이점은 무엇일까?
+
+
+// serialize : 직렬화? / 회원가입 후 비밀번호를 삭제
+UserSchema.methods.serialize = function(){
+    const data = this.toJSON();
+    delete data.hashedPassword;
+    return data;
+}
 
 const User = mongoose.model('User', UserSchema);
 export default User;

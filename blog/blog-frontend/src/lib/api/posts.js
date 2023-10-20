19d7e94a5ff860f90c0810f196e2d70b949496f9
Dongmin Kim <macaw2469@gmail.com>
2023-10-20 14:20:16 +0900

    추가 : 포스트 목록 페이지
        - UI 컴포넌트 구현 : PostList.jsx
        - Common 컴포넌트 : SubInfo, Tags
        - API : api/posts.js - listPosts
        - 리듀서, 사가 적용 : moudules/index.js
        - 컨테이너 구현 : PostListContainer
        - 페이지에 적용 : PostListPage.js

--- blog/blog-frontend/src/lib/api/posts.js
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -4,3 +4,10 @@ export const writePost = ({title, body, tags})=> client.post('/api/posts', {titl
 
 // 포스트 읽기
 export const readPost=(id)=>client.get(`/api/posts/${id}`)
+
+//포스트 리스트 
+export const listPosts = ({page, username, tag})=>{
+    return client.get(`/api/posts`,{
+        params:{page, username, tag}
+    })
+}
---
+++ blog/blog-frontend/src/modules/posts.js
@@ -0,0 +1,41 @@
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts'
+import {takeLatest} from 'redux-saga/effects'
+
+// 액션
+const [LIST_POST, LIST_POST_SUCCESS, LIST_POST_FAILURE] = createRequestActionTypes('posts/LIST_POSTS');
+
+//액션 생성 함수
+export const listPosts = createAction(
+    LIST_POST,
+    ({tag,username,page})=>({tag, username, page})
+)
+
+// 사가
+const listPostSaga = createRequestSaga(LIST_POST, postsAPI.listPosts )
+export function* postsSaga(){
+    yield takeLatest(LIST_POST, listPostSaga)
+}
+
+// 초기 상태
+const initialState={
+    posts:null,
+    error:null,
+}
+
+const posts = handleActions(
+    {
+        [LIST_POST_SUCCESS] : (state, {payload:posts})=>({
+            ...state,
+            posts
+        }),
+        [LIST_POST_FAILURE] : (state, {payload:error})=>({
+            ...state,
+            error,
+        })
+    },
+    initialState
+)
+
+export default posts;
